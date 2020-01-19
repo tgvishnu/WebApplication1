907076313ab2293bdeb9ee55905c5b596d5c87be
tgvishnu <tgvishnuvardhan@gmail.com>
2020-01-19 19:03:59 +0000

    changes updated as per the request

--- WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace WebApplication1.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "my value1", "my value2" };
         }
 
         // GET api/values/5
